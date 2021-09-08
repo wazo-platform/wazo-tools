97cdfeeeecc39362a41801fb641904d7ddd13796
Sebastien Duthil <sduthil@wazo.community>
2021-09-08 17:45:04 -0400

    bus-tools: add send-bus-event

---
+++ bus-tools/send-bus-event.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# Copyright 2021 The Wazo Authors  (see the AUTHORS file)
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import json
+import kombu
+import logging
+import sys
+
+from argparse import ArgumentParser
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(process)d] (%(levelname)s) (%(name)s): %(message)s')
+
+_DEFAULT_CONFIG = {
+    'username': 'guest',
+    'password': 'guest',
+    'host': 'localhost',
+    'vhost': '',
+    'port': 5672,
+    'exchange_name': 'xivo',
+    'exchange_type': 'topic',
+}
+
+
+def main():
+    parser = ArgumentParser('Send message to RabbitMQ exchange xivo')
+    parser.add_argument('-n', '--hostname', help='RabbitMQ server',
+                        default='localhost')
+    parser.add_argument('-p', '--port', help='Port of RabbitMQ',
+                        default='5672')
+    parser.add_argument('-r', '--routing-key', help='Routing key to bind on bus',
+                        dest='routing_key', required=True)
+    parser.add_argument('-i', '--input-event-file', help='JSON file containing the event to send. Default: stdin.',
+                        dest='input_event_file', default='-')
+    args = parser.parse_args()
+
+    config = _DEFAULT_CONFIG
+    config['host'] = args.hostname
+    config['port'] = args.port
+
+    bus_url = 'amqp://{username}:{password}@{host}:{port}/{vhost}'.format(**config)
+    with kombu.Connection(bus_url) as connection:
+        exchange = kombu.Exchange(config['exchange_name'], type=config['exchange_type'])
+        producer = kombu.Producer(connection, exchange=exchange)
+        with sys.stdin if args.input_event_file == '-' else open(args.input_event_file, 'r') as f:
+            event_data = json.load(f)
+        producer.publish(event_data, routing_key=args.routing_key)
+
+
+if __name__ == '__main__':
+    main()
