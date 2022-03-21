318449b7827eff2b238daf962bac26de5fe8ffc0
Sylvain Boily <sylvain@wazo.io>
2022-03-21 08:26:20 -0400

    add support for headers

--- bus-tools/recv-bus-event.py
+++ bus-tools/recv-bus-event.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# Copyright 2015-2018 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2015-2022 The Wazo Authors  (see the AUTHORS file)
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -22,8 +22,8 @@ import logging
 
 from kombu.mixins import ConsumerMixin
 from pprint import pformat
+from kombu import binding as Binding
 
-EXCHANGE = kombu.Exchange('xivo', type='topic')
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(process)d] (%(levelname)s) (%(name)s): %(message)s')
 logger = logging.getLogger(__name__)
@@ -31,12 +31,27 @@ logger = logging.getLogger(__name__)
 
 class C(ConsumerMixin):
 
-    def __init__(self, connection, routing_key):
+    def __init__(self, connection, routing_key, bindings, tenant):
         self.connection = connection
         self.routing_key = routing_key
+        self.bindings = bindings
+        self.tenant = tenant
 
     def get_consumers(self, Consumer, channel):
-        return [Consumer(kombu.Queue(exchange=EXCHANGE, routing_key=self.routing_key, exclusive=True),
+        bindings = []
+        if self.routing_key:
+            print('Use routing key binding')
+            exchange = kombu.Exchange('xivo', type='topic')
+        if self.bindings:
+            print('Use headers binding')
+            exchange = kombu.Exchange('wazo-headers', type='headers')
+            for event in self.bindings.split(','):
+                bindings.append(kombu.binding(
+                    exchange=exchange,
+                    routing_key=None,
+                    arguments={'name': event, 'tenant_uuid': self.tenant},
+                ))
+        return [Consumer(kombu.Queue(exchange=exchange, routing_key=self.routing_key, bindings=bindings, exclusive=True),
                 callbacks=[self.on_message])]
 
     def on_message(self, body, message):
@@ -51,7 +66,11 @@ def main():
     parser.add_argument('-p', '--port', help='Port of RabbitMQ',
                         default='5672')
     parser.add_argument('-r', '--routing-key', help='Routing key to bind on bus',
-                        dest='routing_key', default='#')
+                        dest='routing_key')
+    parser.add_argument('-e', '--event-name', help='Event Name to bind on bus',
+                        dest='event_name')
+    parser.add_argument('-t', '--tenant', help='Tenant UUID to bind on bus',
+                        dest='tenant')
 
     args = parser.parse_args()
 
@@ -59,7 +78,7 @@ def main():
 
     with kombu.Connection(url_amqp) as conn:
         try:
-            C(conn, args.routing_key).run()
+            C(conn, args.routing_key, args.event_name, args.tenant).run()
         except KeyboardInterrupt:
             return
 
