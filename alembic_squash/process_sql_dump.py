28028807455ec44b5f2f029c60b5ff64e7008114
Charles Langlois <charles.langlois@wazo.io>
2025-09-10 14:56:13 -0400

    alembic_squash: fix linting errors

--- alembic_squash/process_sql_dump.py
+++ alembic_squash/process_sql_dump.py
@@ -1,12 +1,15 @@
-import sys
 # import sqlglot
 import re
+import sys
+from collections.abc import Iterator
 
 CHECK_CONSTRAINT = re.compile(r'CONSTRAINT \w+ CHECK \((.*?)\)$')
 MEMBERCHECK = re.compile(
     r"\(\((?P<column>\w+)\).*? = ANY \(\(ARRAY\[(?P<values>.*)\]\).*?\)"
 )
-def process_sql_dump(sql_text: str) -> str:
+
+
+def process_sql_dump(sql_text: str) -> Iterator[str]:
     for line in sql_text.split('\n'):
         match = CHECK_CONSTRAINT.search(line)
 
@@ -16,11 +19,9 @@ def process_sql_dump(sql_text: str) -> str:
             def replace_member_check(match):
                 column = match.group('column')
                 values = match.group('values')
-                values = re.findall(
-                    r"('\w+')[^,]*,?",
-                    values
-                )
+                values = re.findall(r"('\w+')[^,]*,?", values)
                 return f"{column} IN ({', '.join(values)})"
+
             new_line = re.sub(MEMBERCHECK, replace_member_check, line)
             yield new_line
 
